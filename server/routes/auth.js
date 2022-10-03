58145363c39ff89d594f474458b0287648c775dd
KhalilAbbaass <103119609+KhalilAbbaass@users.noreply.github.com>
2022-10-03 09:35:17 +0300

    server api

---
+++ server/routes/auth.js
@@ -0,0 +1,58 @@
+const router = require("express").Router();
+const User = require("../models/User")
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
+
+//REGISTER
+router.post("/register" , async (req,res)=>{
+    const newUser = new User({
+        username:req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+    });
+    try{
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+        console.log(savedUser);
+    } catch (err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+//LOGIN
+router.post("/login" , async(req, res)=>{
+    try{
+        const user = await User.findOne({ username: req.body.username});
+        !user && res.status(401).json("Wrong credentials!")
+        
+        const hashPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC).toString(CryptoJS.enc.Utf8);
+        hashPassword !== req.body.password && res.status(401).json("Wrong password");
+
+//JWt access token
+
+        const accessToken = jwt.sign({
+            id:user._id, 
+            isAdmin: user.isAdmin,
+        },
+         process.env.JWT_SEC,
+          {expiresIn:"3d"}
+         );
+        
+
+//to prevent sending the password even if its encrypted.
+        const { password, ...others} = user._doc;
+
+        res.status(200).json({...others, accessToken});
+
+    }catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+
+
+module.exports = router
